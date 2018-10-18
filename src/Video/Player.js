6b9f9774d4ee3dde3325d20feca61dd7758ad52c
Arissa Fram <arissafram@gmail.com>
2018-10-17 22:14:05 -0400

    init video test app

---
+++ src/Video/Player.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import videojs from 'video-js'
+
+export default class Player extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div>player</div>
+    )
+  }
+}
